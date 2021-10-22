ca0fb3a6a88d41a131b0a728a7a0506f48d513e3
Gerar <Gerar1928@gmail.com>
2021-10-21 23:32:21 -0400

    Adding styling.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,43 @@ import './App.scss';
 
 const App = () => {
   return (
-    <div className='test'>
-      <h1>Hello world</h1>
-      <div className='test2'>
+    <div className='container'>
+      <div id='navbar'>
+        <div className='logo'>Logo</div>
+        <div className='menu'>
+          <ul>
+            <li>
+              <span>Home</span>
+            </li>
+            <li>
+              <span>About</span>
+            </li>
+            <li>
+              <span>Projects</span>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+          </ul>
+        </div>
       </div>
+      <div id='header'>
+        <div id='title'>
+          <p className='name'>Gerar Almonte</p>
+          <p className='profession'>Software engineer</p>
+        </div>
+        <div id='buttons'>
+          <span className='button'>
+            <a href='#'>I need a website</a>
+          </span>
+          <span className='button'>
+            <a href='#'>I'm looking to hire</a>
+          </span>
+        </div>
+      </div>
+      <div className='stars-container-one'></div>
+      <div className='stars-container-two'></div>
+      <div className='stars-container-three'></div>
     </div>
   );
 }
